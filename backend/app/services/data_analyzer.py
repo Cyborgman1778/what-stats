7c66fda40fe6422305aa23ee5b266b78c62a820b
Manel <manelrosaplaya@gmail.com>
2026-04-03 19:27:07 +0200

    streak feature added

--- backend/app/services/data_analyzer.py
+++ backend/app/services/data_analyzer.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 from collections import Counter
 import emoji
+from datetime import timedelta, date
 from typing import Dict, List, Any
 from app.utils.constants import CHAT_STOPWORDS
 from nltk.corpus import stopwords
@@ -15,16 +16,39 @@ def _get_filtered_df_by_user(chat_df: pd.DataFrame, user: str) -> pd.DataFrame:
     """Devuelve un sub-dataframe tomando solo los registros de un usuario en concreto"""
     return chat_df[chat_df['Author'] == user]
 
+def _record_new_streak(finish_date: date, day_lenght: int, in_grace_period: bool = False) -> Dict[str, Any]:
+    """Devuelve un diccionario con inicio, fin, y duracion de una racha para la funcion de rachas"""
+
+    if in_grace_period:
+        finish_date = finish_date - timedelta(days=1)
+
+    init_date = finish_date - timedelta(days=day_lenght - 1)
+
+    return {
+        'start': init_date,
+        'end': finish_date,
+        'duration': day_lenght
+    }
+
+
+def _get_all_chat_users(chat_df: pd.DataFrame) -> List[str]:
+    """
+    Devuelve una lista con todos lo usuarios del chat 
+    OJO: devuelve el telefono si cliente no tiene guardado al usuario en la agenda
+
+    chat_df['Author'].unique() extrae los nombres únicos de la columna 'Author'
+    .dropna() elimina los valores nulos (por ejemplo, mensajes del sistema de WhatsApp)
+    .tolist() lo convierte a una lista normal de Python para que FastAPI pueda serializarlo a JSON
+    """
+
+    return chat_df['Author'].dropna().unique().tolist()
+
 
 def get_basic_stats(chat_df: pd.DataFrame) -> Dict[str, Any]:
     """Calcula mensajes totales y participantes."""
     return {
         "total_messages": len(chat_df),
-
-        # chat_df['Author'].unique() extrae los nombres únicos de la columna 'Author'
-        # .dropna() elimina los valores nulos (por ejemplo, mensajes del sistema de WhatsApp)
-        # .tolist() lo convierte a una lista normal de Python para que FastAPI pueda serializarlo a JSON
-        "participants": chat_df['Author'].dropna().unique().tolist(),
+        "participants": _get_all_chat_users(chat_df),
         "n_participants": chat_df['Author'].nunique()
     }
 
@@ -126,9 +150,12 @@ def get_emoji_stats(chat_df: pd.DataFrame, user: str | None = None, top_n: int =
     emoji_counter = Counter(emoji_list)
     return dict(emoji_counter.most_common(top_n))
 
-#
-def get_length_stats(chat_df: pd.DataFrame, top_n: int = 10) -> List[Dict[str, Any]]:
+# Se puede obtener estadisticas de un solo usuario pasandole este como parametro, si esta vacio se usaran todos los usuarios del chat
+def get_length_stats(chat_df: pd.DataFrame, user: str | None = None, top_n: int = 10) -> List[Dict[str, Any]]:
     """Obtener un top n de los mensajes mas largos, devolviendo el mensaje, la longitud y el autor"""
+
+    if user:
+        chat_df = _get_filtered_df_by_user(chat_df, user)
     
     # Creamos una serie con la longitud de cada mensaje
     lengths = chat_df["Message"].fillna("").str.len()
@@ -141,6 +168,92 @@ def get_length_stats(chat_df: pd.DataFrame, top_n: int = 10) -> List[Dict[str, A
     # Seleccionamos solo las columnas necesarias
     result = chat_df_cropped[["Author", "Message", "Length"]]
     return result.to_dict(orient="records")
+
+#
+def get_streak_stats(chat_df: pd.DataFrame, top_n: int = 3) -> List[Dict[str, Any]]:
+    """
+    Obtener las rachas de dias seguidos con mensajes mas largas del chat.
+
+    Se puede seleccionar un top N o, por defecto, seran las 3 mas altas.
+
+    Se tomaran como rachas si se cumplen las siguientes caracteristicas:
+        - Mas de 1 usuario escribe en el chat cada dia.
+        - No hay un dia sin mensajes en medio.
+        - Como caso limite, se permite un dia de gracia si al menos un usuario ha escrito y, el dia anterior
+          y el siguiente, alguien mas, aparte de ese usuario, ha participado en el chat. (Pueden haber varios dias
+          de gracia dentro de la misma racha pero no seguidos)
+        - Las rachas seran de 3 dias como minimo
+
+    """
+    chat_df_with_date = chat_df.assign(Date=chat_df['Timestamp'].dt.date)
+
+    # Dejamos solo un registro por cada autor/fecha, recortamos el DF para quedarnos solo con esas columnas y lo ordenamos por fechas
+    chat_df_cropped = chat_df_with_date.drop_duplicates(subset=['Author', 'Date'], keep='first')
+    chat_df_sorted = chat_df_cropped[['Author', 'Date']].sort_values(by='Date')
+    chat_df_sorted = chat_df_sorted.reset_index(drop=True)
+
+    streaks = []
+    ant_date = None
+    daily_user_count = 0
+    day_count = 0
+    grace_period = False
+
+    for index, row in chat_df_sorted.iterrows():
+        act_date = row['Date']
+        if index == 0:
+            ant_date = act_date
+            day_count = 0
+            daily_user_count += 1
+            continue
+
+        if act_date != ant_date:
+            if act_date == ant_date + timedelta(days=1):
+                if daily_user_count == 1: #Si el dia anterior solo ha hablado una persona
+                    if grace_period:
+                        if day_count >= 3:
+                            streaks.append(_record_new_streak(ant_date, day_count, grace_period))
+                        day_count = 0
+                        grace_period = False #Se pone false porq ue quiere decir qe ha habido dos dias seguidos con 1 solo usuario
+                    else:
+                        if day_count > 0:
+                            grace_period = True # La unica forma de que grace_period se ponga a True es que el dia anterior haya sido un dia de racha valido (>2 usuarios)
+                        else:
+                            grace_period = False
+                else:
+                    grace_period = False
+
+            daily_user_count = 0
+
+
+        if act_date == ant_date: #Cada vez que entra aqui es un usuario mas que habla
+            daily_user_count += 1
+            if daily_user_count == 2: # Solo entra cuando se llega a 2 usuarios en un dia, no puede entrar mas de 1 vez por dia
+                day_count += 1
+                if grace_period:
+                    day_count += 1
+            grace_period = False
+        elif act_date == ant_date + timedelta(days=1): #Cada vez que entra aqui es que es el primero en hablar despues de un dia donde ya se ha hablado
+            daily_user_count += 1
+        else: #Si entra aqui quiere decir que nadie ha hablado en el dia anterior por tanto se ha roto la racha en ant_date
+            if day_count >= 3:
+                streaks.append(_record_new_streak(ant_date, day_count, grace_period))
+            day_count = 0
+            daily_user_count = 1
+            grace_period = False
+
+        ant_date = act_date
+
+    if day_count >= 3:
+        streaks.append(_record_new_streak(ant_date, day_count, grace_period))
+
+    streaks.sort(key=lambda x: x['duration'], reverse=True)
+
+    return streaks[:top_n]
+
+
+
+
+
     
 
 def analyze_chat_data(chat_df: pd.DataFrame) -> Dict[str, Any]:
